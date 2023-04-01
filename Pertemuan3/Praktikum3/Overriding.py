98948ecdff1a91e954b427fc9f70310a61318d17
andrianshevc <127664420+andrianshevc@users.noreply.github.com>
2023-04-01 20:39:26 +0700

    Add files via upload

---
+++ Pertemuan3/Praktikum3/Overriding.py
@@ -0,0 +1,15 @@
+#Nama : Andrian Shevchenko
+#NIM : 210511071
+#Kelas : R2
+
+class Matematika:
+    def add(self, b, c): 
+        return b + c
+    def add(self, b, c, d=0): 
+        return b + c + d
+mat = Matematika()
+Hitung = mat.add(5, 3, 5) 
+print(Hitung)
+mut = Matematika() 
+Tambah = mut.add(18,10) 
+print(Tambah)
