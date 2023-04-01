ec43da87b67ba8ffa428462bb54ba31c56d7d1de
andrianshevc <127664420+andrianshevc@users.noreply.github.com>
2023-04-01 20:38:34 +0700

    Add files via upload

---
+++ Pertemuan3/Praktikum3/Overload2.py
@@ -0,0 +1,11 @@
+#Nama : Andrian Shevchenko 
+#NIM : 210511071
+#Kelas : R2
+
+print(2 + 7)
+print("Andrian" + " Shevchenko")
+print([2, 1] + [0, 5] + [1, 1] + [0, 7, 1])
+
+print(max(1,2,5,10))
+print(max([1, 2, 10]))
+print(max("Sheva"))
